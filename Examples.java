9457886743faedd554018addc4f64381b27401bc
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2026-04-09 21:28:22 +0300

    Update Examples.java

--- Examples.java
+++ Examples.java
@@ -1,3 +1,15 @@
 /*---------------------------------------------
+isPrime metotunun unit testinin JUnit kullanılarak
+test edilmesi
+----------------------------------------------*/
+class IsPrimeTest {
+  @Test
+  void givenValues_whenHighPrime_thenTrue()
+  {
+    
+  }
+}
+
+/*---------------------------------------------
 BigInteger ve BigDecimal sınıfları
 ----------------------------------------------*/
