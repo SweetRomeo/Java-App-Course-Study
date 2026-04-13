26a5da01f6cf726b9267e336cd4e2350c9577270
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2026-04-14 00:23:56 +0300

    Update Examples.java

--- Examples.java
+++ Examples.java
@@ -26,6 +26,53 @@ class IsPrimeTest {
       assertTrue(NumberUtil.isPrime(val));
   }  
 }
+/*-----------------------------------------------
+StopWatch Sınıfı ve Test kodu
+-----------------------------------------------*/
+package org.csystem.diagnostic;
+
+public class StopWatch {
+  private long m_start;
+  private long m_stop;
+  
+  public StopWatch()
+  {
+    
+  }
+
+  public static StopWatch create()
+  {
+    return new StopWatch();
+  }
+  
+  public static StopWatch createStarted()
+  {
+    return (new StopWatch()).start();
+  }
+
+  public StopWatch start()
+  {
+    m_start = System.nanoTime();
+    return this;
+  }
+
+  public StopWatch stop()
+  {
+    m_stop = System.nanoTime();
+    return this;
+  }
+  
+  public StopWatch reset()
+  {
+    m_start = m_stop = 0;
+    return this;
+  }
+
+  public long elapsedTime(TimeUnit timeUnit)
+  {
+    return timeUnit.convert(m_stop - m_start, TimeUnit.NANOSECONDS);
+  }  
+}
 
 /*---------------------------------------------
 BigInteger ve BigDecimal sınıfları
